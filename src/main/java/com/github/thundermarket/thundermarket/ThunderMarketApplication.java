a3ec67ff85a21f6f9e0eaffdc4f65e98e1e03923
jjkim01 <jaen6563@gmail.com>
2024-05-18 16:11:12 +0900

    프로젝트 기본 구성 추가

---
+++ src/main/java/com/github/thundermarket/thundermarket/ThunderMarketApplication.java
@@ -0,0 +1,13 @@
+package com.github.thundermarket.thundermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThunderMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThunderMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/thundermarket/thundermarket/ThunderMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.thundermarket.thundermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThunderMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
