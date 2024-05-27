ca6de8f54792ea63ec4bcd0e56fbd55e0103469c
jjkim01 <jaen6563@gmail.com>
2024-05-27 23:20:36 +0900

    feature: 회원 DTO 추가

---
+++ src/main/java/com/github/thundermarket/thundermarket/domain/User.java
@@ -0,0 +1,32 @@
+package com.github.thundermarket.thundermarket.domain;
+
+public class User {
+
+    private Long id;
+    private String userId;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
