aecd91fde997391344bef4d3c3b879a59b928e08
jjkim01 <jaen6563@gmail.com>
2024-05-27 23:19:23 +0900

    feature: 회원 정보 저장 기능 검증을 위한 InMemoryUserRepository 추가

---
+++ src/main/java/com/github/thundermarket/thundermarket/repository/InMemoryUserRepository.java
@@ -0,0 +1,35 @@
+package com.github.thundermarket.thundermarket.repository;
+
+import com.github.thundermarket.thundermarket.domain.User;
+import org.springframework.stereotype.Repository;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class InMemoryUserRepository implements UserRepository {
+
+    private static final Map<Long, User> inMemoryUserStore = new HashMap<>();
+    private static long id = 1L;
+
+    @Override
+    public User save(User user) {
+        user.setId(id++);
+
+        inMemoryUserStore.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User findByUserIdAndPassword(String userId, String password) {
+        for (User user : inMemoryUserStore.values()) {
+            if(user.getUserId().equals(userId) && user.getPassword().equals(password)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public void deleteAll() {
+        inMemoryUserStore.clear();
+    }
+}
