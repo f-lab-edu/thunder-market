b9d9c24054596dd1484e703dc7ebd9f4b882a566
jjkim01 <jaen6563@gmail.com>
2024-05-27 23:23:25 +0900

    test: 회원가입 컨트롤러 post 요청 테스트코드 추가

---
+++ src/test/java/com/github/thundermarket/thundermarket/UserControllerTest.java
@@ -0,0 +1,58 @@
+package com.github.thundermarket.thundermarket;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.thundermarket.thundermarket.domain.User;
+import com.github.thundermarket.thundermarket.repository.InMemoryUserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    InMemoryUserRepository userRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        userRepository = new InMemoryUserRepository();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void 회원가입_성공() throws Exception {
+        String userId = "test01";
+        String password = "password";
+
+        User user = new User();
+        user.setUserId(userId);
+        user.setPassword(password);
+
+        String userJson = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/join")
+                        .contentType("application/json")
+                        .content(userJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId").value("test01"))
+                .andExpect(jsonPath("$.password").value("password"));
+    }
+}
