6f43a6a3fb4a576e6b39dfccf59b356166e4da6c
jjkim01 <jaen6563@gmail.com>
2024-05-28 11:15:45 +0900

    feature: Controller에 사용자 입력값에 대한 이메일 형식 검증 로직 추가

--- src/main/java/com/github/thundermarket/thundermarket/controller/UserController.java
+++ src/main/java/com/github/thundermarket/thundermarket/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.github.thundermarket.thundermarket.controller;
 
+import com.github.thundermarket.thundermarket.Util.EmailValidator;
 import com.github.thundermarket.thundermarket.domain.User;
 import com.github.thundermarket.thundermarket.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,12 +22,15 @@ public class UserController {
     }
 
     @PostMapping
-    public User join(@RequestBody User user) {
-        return userService.join(user);
+    public ResponseEntity<?> join(@RequestBody User user) {
+        if(!EmailValidator.isValid(user.getEmail())) {
+            return new ResponseEntity<>(EmailValidator.NOT_VALID_EMAIL_MESSAGE, HttpStatus.BAD_REQUEST);
+        };
+        return new ResponseEntity<>(userService.join(user), HttpStatus.OK);
     }
 
     @GetMapping
-    public List<User> findAllUsers() {
-        return userService.findAllUsers();
+    public ResponseEntity<?> findAllUsers() {
+        return new ResponseEntity<>(userService.findAllUsers(), HttpStatus.OK);
     }
 }
