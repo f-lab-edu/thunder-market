b23478ecffe110660dcd74379df2d0df81495a7a
jjkim01 <jaen6563@gmail.com>
2024-06-10 22:01:22 +0900

    test: 상품 관련 비즈니스 로직 처리를 위한 서비스 계층 추가

---
+++ src/main/java/com/github/thundermarket/thundermarket/service/ProductService.java
@@ -0,0 +1,12 @@
+package com.github.thundermarket.thundermarket.service;
+
+import com.github.thundermarket.thundermarket.domain.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductService {
+    public List<Product> getProductList() {
+        return new ArrayList<>();
+    }
+}
--- src/test/java/com/github/thundermarket/thundermarket/ProductServiceTest.java
+++ src/test/java/com/github/thundermarket/thundermarket/ProductServiceTest.java
@@ -1,17 +1,20 @@
 package com.github.thundermarket.thundermarket;
 
 import com.github.thundermarket.thundermarket.domain.Product;
+import com.github.thundermarket.thundermarket.service.ProductService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ProductServiceTest {
 
+    private final ProductService productService = new ProductService();
+
     @Test
     public void 상품0개_상품목록_조회() {
-        List<Product> productList = new ArrayList<>(); 
+        List<Product> productList = productService.getProductList();
+
         Assertions.assertThat(productList.isEmpty()).isEqualTo(true);
     }
 
