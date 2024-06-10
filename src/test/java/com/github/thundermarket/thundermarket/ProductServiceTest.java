0e4044dce02db5f8af5a6c611ff2502cc6f21993
jjkim01 <jaen6563@gmail.com>
2024-06-10 21:52:11 +0900

    test: 상품 조회 기능 관련 use case 테스트코드에 추가

---
+++ src/test/java/com/github/thundermarket/thundermarket/ProductServiceTest.java
@@ -0,0 +1,42 @@
+package com.github.thundermarket.thundermarket;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProductServiceTest {
+
+    @Test
+    public void 상품0개_상품목록_조회() {
+        Assertions.assertThat(1).isEqualTo(2);
+    }
+
+    @Test
+    public void 상품1개_상품목록_조회() {
+        Assertions.assertThat(1).isEqualTo(2);
+    }
+
+    @Test
+    public void 상품2개_상품목록_조회() {
+        Assertions.assertThat(1).isEqualTo(2);
+    }
+
+    @Test
+    public void 상품1개_상품목록_조회_후_상품1개추가_다시조회() {
+        Assertions.assertThat(1).isEqualTo(2);
+    }
+
+    @Test
+    public void 상품1개_상품목록_조회_후_상품정보수정_다시조회() {
+        Assertions.assertThat(1).isEqualTo(2);
+    }
+
+    @Test
+    public void 상품1개_상품목록_조회_후_상품삭제_다시조회() {
+        Assertions.assertThat(1).isEqualTo(2);
+    }
+
+    @Test
+    public void 상품2개_상품목록_조회_후_상품삭제_다시조회() {
+        Assertions.assertThat(1).isEqualTo(2);
+    }
+}
