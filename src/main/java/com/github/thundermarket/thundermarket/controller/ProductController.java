3157258ddb7bf46b21fd5ea75b28574aa15b6bbd
jjkim01 <jaen6563@gmail.com>
2024-06-12 19:41:14 +0900

    feature: 상품상세정보 controller, repository 추가

--- src/main/java/com/github/thundermarket/thundermarket/controller/ProductController.java
+++ src/main/java/com/github/thundermarket/thundermarket/controller/ProductController.java
@@ -3,12 +3,15 @@ package com.github.thundermarket.thundermarket.controller;
 import com.github.thundermarket.thundermarket.domain.Product;
 import com.github.thundermarket.thundermarket.exception.ErrorMessage;
 import com.github.thundermarket.thundermarket.exception.UnauthenticatedException;
+import com.github.thundermarket.thundermarket.domain.ProductDetail;
+import com.github.thundermarket.thundermarket.service.ProductDetailService;
 import com.github.thundermarket.thundermarket.service.ProductService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.naming.AuthenticationException;
@@ -18,14 +21,29 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
+    private final ProductDetailService productDetailService;
 
     @Autowired
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, ProductDetailService productDetailService) {
         this.productService = productService;
+        this.productDetailService = productDetailService;
     }
 
     @GetMapping("/api/v1/products")
     public ResponseEntity<List<Product>> getProductList() {
         return new ResponseEntity<>(productService.getProductList(), HttpStatus.OK);
+    }
+
+    @GetMapping("/api/v1/products/{id}")
+    public ResponseEntity<?> getProductDetail(@PathVariable("id") Long id, HttpSession session) {
+        String userEmail = (String) session.getAttribute("userEmail");
+        if (userEmail == null) {
+            return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
+        }
+        ProductDetail productDetail = productDetailService.getProductDetail(id);
+        if (productDetail == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(productDetail, HttpStatus.OK);
     }
 }
--- src/main/java/com/github/thundermarket/thundermarket/repository/MySQLProductDetailRepository.java
+++ src/main/java/com/github/thundermarket/thundermarket/repository/MySQLProductDetailRepository.java
@@ -1,17 +1,67 @@
 package com.github.thundermarket.thundermarket.repository;
 
 import com.github.thundermarket.thundermarket.domain.ProductDetail;
+import com.github.thundermarket.thundermarket.domain.User;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.*;
 
 @Repository
 public class MySQLProductDetailRepository implements ProductDetailRepository {
+
+    private final DataSource dataSource;
+
+    public MySQLProductDetailRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    private Connection getConnection() {
+        return DataSourceUtils.getConnection(dataSource);
+    }
+
+    private void releaseConnection(Connection conn) {
+        DataSourceUtils.releaseConnection(conn, dataSource);
+    }
+
     @Override
     public ProductDetail save(ProductDetail productDetail) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public ProductDetail findById(Long productId) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public ProductDetail findById(Long id) {
+        String sql = "SELECT * FROM productsDetails WHERE id = ?";
+        Connection conn = null;
+
+        try {
+            conn = getConnection();
+            try (PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                ps.setLong(1, id);
+
+                try (ResultSet resultSet = ps.executeQuery()) {
+                    if (resultSet.next()) {
+                        ProductDetail productDetail = new ProductDetail();
+                        productDetail.setColor(resultSet.getString("color"));
+                        productDetail.setProductCondition(resultSet.getString("productCondition"));
+                        productDetail.setBatteryCondition(resultSet.getString("batteryCondition"));
+                        productDetail.setCameraCondition(resultSet.getString("cameraCondition"));
+                        productDetail.setAccessories(resultSet.getString("accessories"));
+                        productDetail.setPurchaseDate(resultSet.getString("purchaseDate"));
+                        productDetail.setWarrantyDuration(resultSet.getString("warrantyDuration"));
+                        productDetail.setTradeLocation(resultSet.getString("tradeLocation"));
+                        productDetail.setDeliveryFee(resultSet.getInt("deliveryFee"));
+
+                        return productDetail;
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Find by id failed", e);
+        } finally {
+            releaseConnection(conn);
+        }
+        return null;
     }
 }
