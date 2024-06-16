daaf4fdb9422e15bf79c3e136882a6df091d575e
jjkim01 <jaen6563@gmail.com>
2024-06-16 19:44:58 +0900

    refactor: 로그인 세션이 없으면 예외처리로 로직 분리, 반환타입에 와일드카드 제거

--- src/main/java/com/github/thundermarket/thundermarket/controller/ProductController.java
+++ src/main/java/com/github/thundermarket/thundermarket/controller/ProductController.java
@@ -1,5 +1,8 @@
 package com.github.thundermarket.thundermarket.controller;
 
+import com.github.thundermarket.thundermarket.domain.Product;
+import com.github.thundermarket.thundermarket.exception.ErrorMessage;
+import com.github.thundermarket.thundermarket.exception.UnauthenticatedException;
 import com.github.thundermarket.thundermarket.service.ProductService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.naming.AuthenticationException;
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -19,10 +25,10 @@ public class ProductController {
     }
 
     @GetMapping("/api/v1/products")
-    public ResponseEntity<?> getProductList(HttpSession session) {
+    public ResponseEntity<List<Product>> getProductList(HttpSession session) {
         String userEmail = (String) session.getAttribute("userEmail");
         if (userEmail == null) {
-            return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
+            throw new UnauthenticatedException(ErrorMessage.FORBIDDEN.toString());
         }
         return new ResponseEntity<>(productService.getProductList(), HttpStatus.OK);
     }
