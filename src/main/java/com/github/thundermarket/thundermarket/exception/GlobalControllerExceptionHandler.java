7a5023be3c61f55942eac8109139bf76bb70fe27
jjkim01 <jaen6563@gmail.com>
2024-06-16 20:13:45 +0900

    refactor: 컨트롤러 예외 핸들러에 패키지 범위 지정

--- src/main/java/com/github/thundermarket/thundermarket/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/com/github/thundermarket/thundermarket/exception/GlobalControllerExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "com.github.thundermarket.thundermarket.controller")
 public class GlobalControllerExceptionHandler {
 
     @ExceptionHandler
