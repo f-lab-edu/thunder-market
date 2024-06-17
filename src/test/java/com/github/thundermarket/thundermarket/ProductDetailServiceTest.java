bbbfd11654d9758c1671873c91aeafe368b803e0
jjkim01 <jaen6563@gmail.com>
2024-06-17 22:39:26 +0900

    test: Mock을 Stub으로 변경 및 Mock에 사용한 공통동작 제거

--- src/test/java/com/github/thundermarket/thundermarket/ProductDetailServiceTest.java
+++ src/test/java/com/github/thundermarket/thundermarket/ProductDetailServiceTest.java
@@ -1,59 +1,31 @@
 package com.github.thundermarket.thundermarket;
 
 import com.github.thundermarket.thundermarket.domain.ProductDetail;
+import com.github.thundermarket.thundermarket.domain.ProductDetailResponse;
 import com.github.thundermarket.thundermarket.exception.ResourceNotFoundException;
-import com.github.thundermarket.thundermarket.repository.ProductDetailRepository;
 import com.github.thundermarket.thundermarket.service.ProductDetailService;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 public class ProductDetailServiceTest {
 
-    @Mock
-    ProductDetailRepository productDetailRepository;
-
-    @InjectMocks
-    ProductDetailService productDetailService;
-
-    private AutoCloseable autoCloseable;
-
-    @BeforeEach
-    public void setUp() {
-        autoCloseable = MockitoAnnotations.openMocks(this);
-    }
-
-    @AfterEach
-    void tearDown() throws Exception {
-        autoCloseable.close();
-    }
-
     @Test
     public void 상품0개_상세정보_조회() {
-        Long productId = 1L;
-        when(productDetailRepository.findById(productId)).thenReturn(null);
+        Long productId = 0L;
+        ProductDetailService productDetailService = new ProductDetailService(new ProductDetailRepositoryStub());
 
         Assertions.assertThatThrownBy(() -> productDetailService.getProductDetail(productId)).isInstanceOf(ResourceNotFoundException.class);
     }
 
     @Test
     public void 상품1개_상세정보_조회() {
-        String targetColor = "white";
+        ProductDetailService productDetailService = new ProductDetailService(new ProductDetailRepositoryStub());
+        ProductDetail productDetail = new ProductDetail("white", "", "", "", "", "", "", "", 0);
+        ProductDetailResponse expectedProductDetail = productDetail.toResponse();
         Long productId = 1L;
-        ProductDetail productDetailForMock = new ProductDetail();
-        productDetailForMock.setColor("white");
-        when(productDetailRepository.findById(productId)).thenReturn(productDetailForMock);
 
-        ProductDetail productDetail = productDetailService.getProductDetail(productId);
+        ProductDetailResponse productDetailResponse = productDetailService.getProductDetail(productId);
 
-        verify(productDetailRepository).findById(productId);
-        Assertions.assertThat(productDetail.getColor()).isEqualTo(targetColor);
+        Assertions.assertThat(productDetailResponse).isEqualTo(expectedProductDetail);
     }
 }
