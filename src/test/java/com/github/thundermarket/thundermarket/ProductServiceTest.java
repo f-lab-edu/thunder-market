717fe9a21e7d7f347bd61e28815b528fe0f95226
jjkim01 <jaen6563@gmail.com>
2024-06-17 11:21:01 +0900

    test: 테스트 코드에서 getter 대신 객체 비교하도록 변경

--- src/test/java/com/github/thundermarket/thundermarket/ProductServiceTest.java
+++ src/test/java/com/github/thundermarket/thundermarket/ProductServiceTest.java
@@ -27,84 +27,92 @@ public class ProductServiceTest {
 
     @Test
     public void 상품1개_상품목록_조회() {
+        Long id = 0L;
         String name = "아이폰11";
         int price = 200000;
         String status = "판매중";
-        Product product = new Product(name, price, status);
+        Product product = new Product(id, name, price, status);
         productService.addProduct(product);
 
         List<Product> productList = productService.getProductList();
 
-        Assertions.assertThat(productList.getFirst().getName()).isEqualTo(name);
+        Assertions.assertThat(productList.getFirst()).isEqualTo(product);
     }
 
     @Test
     public void 상품2개_상품목록_조회() {
+        Long id = 0L;
         String name = "아이폰11";
         int price = 200000;
         String status = "판매중";
-        Product product = new Product(name, price, status);
+        Product product = new Product(id, name, price, status);
         productService.addProduct(product);
 
+        Long id2 = 1L;
         String name2 = "아이폰12";
         int price2 = 300000;
         String status2 = "판매중";
-        Product product2 = new Product(name2, price2, status2);
+        Product product2 = new Product(id2, name2, price2, status2);
         productService.addProduct(product2);
 
         List<Product> productList = productService.getProductList();
 
-        Assertions.assertThat(productList.getFirst().getName()).isEqualTo(name);
-        Assertions.assertThat(productList.get(1).getName()).isEqualTo(name2);
+        Assertions.assertThat(productList.getFirst()).isEqualTo(product);
+        Assertions.assertThat(productList.get(1)).isEqualTo(product2);
     }
 
     @Test
     public void 상품1개_상품목록_조회_후_상품1개추가_다시조회() {
+        Long id = 0L;
         String name = "아이폰11";
         int price = 200000;
         String status = "판매중";
-        Product product = new Product(name, price, status);
+        Product product = new Product(id, name, price, status);
         productService.addProduct(product);
         List<Product> productList = productService.getProductList();
-        Assertions.assertThat(productList.getFirst().getName()).isEqualTo(name);
+        Assertions.assertThat(productList.getFirst()).isEqualTo(product);
 
+        Long id2 = 1L;
         String name2 = "아이폰12";
         int price2 = 300000;
         String status2 = "판매중";
-        Product product2 = new Product(name2, price2, status2);
+        Product product2 = new Product(id2, name2, price2, status2);
         productService.addProduct(product2);
         List<Product> productList2 = productService.getProductList();
-        Assertions.assertThat(productList2.get(1).getName()).isEqualTo(name2);
+        Assertions.assertThat(productList2.get(1)).isEqualTo(product2);
     }
 
     @Test
     public void 상품1개_상품목록_조회_후_상품정보수정_다시조회() {
+        Long id = 0L;
         String name = "아이폰11";
         int price = 200000;
         String status = "판매중";
-        Product product = new Product(name, price, status);
+        Product product = new Product(id, name, price, status);
         productService.addProduct(product);
         List<Product> productList = productService.getProductList();
-        Assertions.assertThat(productList.getFirst().getName()).isEqualTo(name);
+        Assertions.assertThat(productList.getFirst()).isEqualTo(product);
 
+        Long id2 = 1L;
         String name2 = "아이폰12";
         int price2 = 300000;
         String status2 = "판매중";
-        Product product2 = new Product(name2, price2, status2);
+        Product product2 = new Product(id2, name2, price2, status2);
         productService.editProduct(product2);
         List<Product> productList2 = productService.getProductList();
-        Assertions.assertThat(productList2.getFirst().getName()).isEqualTo(name2);
+        Assertions.assertThat(productList2.getFirst()).isEqualTo(product2);
     }
 
     @Test
     public void 상품1개_상품목록_조회_후_상품삭제_다시조회() {
+        Long id = 0L;
         String name = "아이폰11";
         int price = 200000;
         String status = "판매중";
-        Product product = new Product(name, price, status);
+        Product product = new Product(id, name, price, status);
         productService.addProduct(product);
         List<Product> productList = productService.getProductList();
-        Assertions.assertThat(productList.getFirst().getName()).isEqualTo(name);
+        Assertions.assertThat(productList.getFirst()).isEqualTo(product);
 
         productService.deleteProduct(0L);
         Assertions.assertThat(productList.isEmpty()).isEqualTo(true);
@@ -112,24 +120,26 @@ public class ProductServiceTest {
 
     @Test
     public void 상품2개_상품목록_조회_후_상품삭제_다시조회() {
+        Long id = 0L;
         String name = "아이폰11";
         int price = 200000;
         String status = "판매중";
-        Product product = new Product(name, price, status);
+        Product product = new Product(id, name, price, status);
         productService.addProduct(product);
 
+        Long id2 = 1L;
         String name2 = "아이폰12";
         int price2 = 300000;
         String status2 = "판매중";
-        Product product2 = new Product(name2, price2, status2);
+        Product product2 = new Product(id2, name2, price2, status2);
         productService.addProduct(product2);
 
         List<Product> productList = productService.getProductList();
 
-        Assertions.assertThat(productList.getFirst().getName()).isEqualTo(name);
-        Assertions.assertThat(productList.get(1).getName()).isEqualTo(name2);
+        Assertions.assertThat(productList.getFirst()).isEqualTo(product);
+        Assertions.assertThat(productList.get(1)).isEqualTo(product2);
 
         productService.deleteProduct(0L);
-        Assertions.assertThat(productList.getFirst().getName()).isEqualTo(name2);
+        Assertions.assertThat(productList.getFirst()).isEqualTo(product2);
     }
 }
