7fe0a3fb8882bee52023d12397807c2ab8d55e49
jjkim01 <jaen6563@gmail.com>
2024-06-17 18:11:11 +0900

    refactor: 세션 체크 로직 AOP로 분리

--- src/main/java/com/github/thundermarket/thundermarket/controller/ProductController.java
+++ src/main/java/com/github/thundermarket/thundermarket/controller/ProductController.java
@@ -1,20 +1,15 @@
 package com.github.thundermarket.thundermarket.controller;
 
 import com.github.thundermarket.thundermarket.domain.Product;
-import com.github.thundermarket.thundermarket.exception.ErrorMessage;
-import com.github.thundermarket.thundermarket.exception.UnauthenticatedException;
 import com.github.thundermarket.thundermarket.domain.ProductDetail;
 import com.github.thundermarket.thundermarket.service.ProductDetailService;
 import com.github.thundermarket.thundermarket.service.ProductService;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.naming.AuthenticationException;
 import java.util.List;
 
 @RestController
@@ -35,11 +30,7 @@ public class ProductController {
     }
 
     @GetMapping("/api/v1/products/{id}")
-    public ResponseEntity<?> getProductDetail(@PathVariable("id") Long id, HttpSession session) {
-        String userEmail = (String) session.getAttribute("userEmail");
-        if (userEmail == null) {
-            return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
-        }
+    public ResponseEntity<?> getProductDetail(@PathVariable("id") Long id) {
         ProductDetail productDetail = productDetailService.getProductDetail(id);
         if (productDetail == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
