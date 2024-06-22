4990f7a4da09f36568a4ff1baf41727a173fbef8
jjkim01 <jaen6563@gmail.com>
2024-06-22 15:37:11 +0900

    refactor: 세션 인증 건너뛰는 어노테이션 이름을 긍정적, 추상적으로 변경

--- src/main/java/com/github/thundermarket/thundermarket/aspect/SessionCheckAspect.java
+++ src/main/java/com/github/thundermarket/thundermarket/aspect/SessionCheckAspect.java
@@ -17,7 +17,7 @@ public class SessionCheckAspect {
         this.session = session;
     }
 
-    @Before("execution(* com.github.thundermarket.thundermarket.controller.*.*(..)) && !@annotation(com.github.thundermarket.thundermarket.aspect.AvailableWithoutSession)")
+    @Before("execution(* com.github.thundermarket.thundermarket.controller.*.*(..)) && !@annotation(com.github.thundermarket.thundermarket.aspect.Authenticated)")
     public void checkSession() {
         String userEmail = (String) session.getAttribute("userEmail");
         if (userEmail == null) {
