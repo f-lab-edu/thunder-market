1b854b9398090fdb39d788689da4473cf38906cf
jjkim01 <jaen6563@gmail.com>
2024-06-24 17:37:20 +0900

    test: 상품 등록 성공 테스트 구현

--- src/test/java/com/github/thundermarket/thundermarket/ProductControllerTest.java
+++ src/test/java/com/github/thundermarket/thundermarket/ProductControllerTest.java
@@ -1,22 +1,16 @@
 package com.github.thundermarket.thundermarket;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.thundermarket.thundermarket.domain.Product;
-import com.github.thundermarket.thundermarket.domain.User;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -38,5 +32,36 @@ public class ProductControllerTest {
                 .andExpect(content().string("[]"));
     }
 
-    // @Todo: 상품 등록, 수정, 삭제 구현 후 테스트케이스 추가하기
+    @Test
+    public void 상품_등록_성공() throws Exception {
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("userEmail", "test01@test.com");
+
+        String productRequestJson = "{\n" +
+                "  \"product\": {\n" +
+                "    \"name\": \"iPhone11\",\n" +
+                "    \"price\": 200000,\n" +
+                "    \"status\": \"available\"\n" +
+                "  },\n" +
+                "  \"productDetail\": {\n" +
+                "    \"color\": \"white\",\n" +
+                "    \"productCondition\": \"New\",\n" +
+                "    \"batteryCondition\": \"Good\",\n" +
+                "    \"cameraCondition\": \"Good\",\n" +
+                "    \"accessories\": \"Charger, Earphones\",\n" +
+                "    \"purchaseDate\": \"2023-01-01\",\n" +
+                "    \"warrantyDuration\": \"12 months\",\n" +
+                "    \"tradeLocation\": \"Seoul\",\n" +
+                "    \"deliveryFee\": 5000\n" +
+                "  }\n" +
+                "}";
+
+        mockMvc.perform(post("/api/v1/products")
+                        .contentType("application/json")
+                        .session(session)
+                        .content(productRequestJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.product.name").value("iPhone11"))
+                .andExpect(jsonPath("$.productDetail.color").value("white"));
+    }
 }
