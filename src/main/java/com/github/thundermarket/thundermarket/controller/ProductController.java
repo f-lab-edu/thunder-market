43b3c9586c532cf17ed84274f35ddcca3237a7fb
jjkim01 <jaen6563@gmail.com>
2024-06-24 17:37:39 +0900

    feature: 컨트롤러에 상품 등록 API 추가, ProductRequest 객체 추가

--- src/main/java/com/github/thundermarket/thundermarket/controller/ProductController.java
+++ src/main/java/com/github/thundermarket/thundermarket/controller/ProductController.java
@@ -1,13 +1,14 @@
 package com.github.thundermarket.thundermarket.controller;
 
 import com.github.thundermarket.thundermarket.domain.Product;
+import com.github.thundermarket.thundermarket.domain.ProductRequest;
+import com.github.thundermarket.thundermarket.domain.ProductResponse;
 import com.github.thundermarket.thundermarket.service.ProductDetailService;
 import com.github.thundermarket.thundermarket.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -29,5 +30,10 @@ public class ProductController {
     @GetMapping("/api/v1/products/{id}")
     public ResponseEntity<?> productDetail(@PathVariable("id") Long id) {
         return new ResponseEntity<>(productDetailService.productDetail(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/api/v1/products")
+    public ResponseEntity<ProductResponse> add(@RequestBody ProductRequest productRequest) {
+        return new ResponseEntity<>(productService.add(productRequest.toProduct(), productRequest.toProductDetail()), HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/github/thundermarket/thundermarket/domain/ProductRequest.java
@@ -0,0 +1,45 @@
+package com.github.thundermarket.thundermarket.domain;
+
+public class ProductRequest {
+
+    private Product product;
+    private ProductDetail productDetail;
+
+    private ProductRequest(Product product, ProductDetail productDetail) {
+        this.product = product;
+        this.productDetail = productDetail;
+    }
+
+    public ProductRequest() {
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public ProductDetail getProductDetail() {
+        return productDetail;
+    }
+
+    public Product toProduct() {
+        return new Product.Builder()
+                .withName(product.getName())
+                .withPrice(product.getPrice())
+                .withStatus(product.getStatus())
+                .build();
+    }
+
+    public ProductDetail toProductDetail() {
+        return new ProductDetail.Builder()
+                .withColor(productDetail.getColor())
+                .withProductCondition(productDetail.getProductCondition())
+                .withBatteryCondition(productDetail.getBatteryCondition())
+                .withCameraCondition(productDetail.getCameraCondition())
+                .withAccessories(productDetail.getAccessories())
+                .withPurchaseDate(productDetail.getPurchaseDate())
+                .withWarrantyDuration(productDetail.getWarrantyDuration())
+                .withTradeLocation(productDetail.getTradeLocation())
+                .withDeliveryFee(productDetail.getDeliveryFee())
+                .build();
+    }
+}
