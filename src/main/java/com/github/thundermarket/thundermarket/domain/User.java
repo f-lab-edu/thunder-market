e0e0827f39333b921bfacbdd7d5136fc3219ca79
jjkim01 <jaen6563@gmail.com>
2024-06-24 14:45:29 +0900

    refactor: User에 빌더 추가, 수정자를 빌더로 변경

--- src/main/java/com/github/thundermarket/thundermarket/domain/User.java
+++ src/main/java/com/github/thundermarket/thundermarket/domain/User.java
@@ -43,4 +43,34 @@ public class User {
     public boolean isEmailValid() {
         return Email.isValid(this.email);
     }
+
+    private User(Builder builder) {
+        this.id = builder.id;
+        this.email = builder.email;
+        this.password = builder.password;
+    }
+
+    public static class Builder {
+        private Long id;
+        private String email;
+        private String password;
+
+        public User.Builder withId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public User.Builder withEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public User.Builder withPassword(String password) {
+            this.password = password;
+            return this;
+        }
+        public User build() {
+            return new User(this);
+        }
+    }
 }
