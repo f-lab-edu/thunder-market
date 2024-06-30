26e8dc7a1e7a9dd3e18bf9975e718db0608545be
jjkim01 <jaen6563@gmail.com>
2024-06-30 19:03:18 +0900

    feature: redis 세션 사용을 위한 설정파일 추가

---
+++ src/main/java/com/github/thundermarket/thundermarket/config/RedisSessionConfig.java
@@ -0,0 +1,19 @@
+package com.github.thundermarket.thundermarket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisSessionConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+}
