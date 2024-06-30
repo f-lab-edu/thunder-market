9e3d63f946bec8e10c0640f6f2d52e53e0cee3eb
jjkim01 <jaen6563@gmail.com>
2024-06-30 19:04:30 +0900

    test: 테스트 의존 인프라 h2, redis를 testcontainers로 대체

--- src/test/java/com/github/thundermarket/thundermarket/ProductDetailControllerTest.java
+++ src/test/java/com/github/thundermarket/thundermarket/ProductDetailControllerTest.java
@@ -1,46 +1,118 @@
 package com.github.thundermarket.thundermarket;
 
-import com.github.thundermarket.thundermarket.constant.SessionConst;
-import com.github.thundermarket.thundermarket.domain.SessionUser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.thundermarket.thundermarket.domain.User;
+import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Testcontainers
 public class ProductDetailControllerTest {
+
+    @Container
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test")
+            .withInitScript("schema.sql");
+
+    @DynamicPropertySource
+    static void registerMySQLProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
+
+    @Container
+    public static GenericContainer redis = new GenericContainer(DockerImageName.parse("redis:6.2-alpine"))
+            .withExposedPorts(6379);
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port", redis::getFirstMappedPort);
+    }
 
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private User createUser(String email, String password) {
+        return new User.Builder()
+                .withEmail(email)
+                .withPassword(password)
+                .build();
+    }
+
     @Test
     public void 상품상세정보_존재하지않으면_404응답() throws Exception {
-        MockHttpSession session = new MockHttpSession();
-        session.setAttribute(SessionConst.SESSION_USER, new SessionUser());
+        User user = createUser("test01@email.com", "password");
+        String userJson = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/api/v1/auth/join")
+                        .contentType("application/json")
+                        .content(userJson))
+                .andExpect(status().isOk());
+
+        MvcResult loginResult = mockMvc.perform(post("/api/v1/auth/login")
+                        .contentType("application/json")
+                        .content(userJson))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Login successful"))
+                .andReturn();
+
+        String sessionId = loginResult.getResponse().getCookie("SESSION").getValue();
 
         mockMvc.perform(get("/api/v1/products/0")
                         .contentType("application/json")
-                        .session(session))
+                        .cookie(new Cookie("SESSION", sessionId)))
                 .andExpect(status().isNotFound());
     }
 
     @Test
     @Sql("/productDetailControllerTest.sql")
     public void 상품상세정보_존재하면_200응답() throws Exception {
-        MockHttpSession session = new MockHttpSession();
-        session.setAttribute(SessionConst.SESSION_USER, new SessionUser());
+        User user = createUser("test01@email.com", "password");
+
+        String userJson = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(post("/api/v1/auth/join")
+                        .contentType("application/json")
+                        .content(userJson))
+                .andExpect(status().isOk());
+
+        MvcResult loginResult = mockMvc.perform(post("/api/v1/auth/login")
+                        .contentType("application/json")
+                        .content(userJson))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Login successful"))
+                .andReturn();
+
+        String sessionId = loginResult.getResponse().getCookie("SESSION").getValue();
 
         mockMvc.perform(get("/api/v1/products/1")
                         .contentType("application/json")
-                        .session(session))
+                        .cookie(new Cookie("SESSION", sessionId)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.productDetail.color").value("white"));
     }
--- src/test/java/com/github/thundermarket/thundermarket/UserControllerTest.java
+++ src/test/java/com/github/thundermarket/thundermarket/UserControllerTest.java
@@ -2,15 +2,24 @@ package com.github.thundermarket.thundermarket;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.thundermarket.thundermarket.domain.User;
+import jakarta.servlet.http.Cookie;
 import org.assertj.core.api.Assertions;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -19,7 +28,33 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@Testcontainers
+@ActiveProfiles("test")
 public class UserControllerTest {
+
+    @Container
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test")
+            .withInitScript("schema.sql");
+
+    @DynamicPropertySource
+    static void registerMySQLProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
+
+    @Container
+    public static GenericContainer redis = new GenericContainer(DockerImageName.parse("redis:6.2-alpine"))
+            .withExposedPorts(6379);
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port", redis::getFirstMappedPort);
+    }
 
     @Autowired
     private MockMvc mockMvc;
@@ -69,18 +104,18 @@ public class UserControllerTest {
                         .content(userJson))
                 .andExpect(status().isOk());
 
-        MockHttpSession session = new MockHttpSession();
-
-        mockMvc.perform(post("/api/v1/auth/login")
+        MvcResult loginResult = mockMvc.perform(post("/api/v1/auth/login")
                         .contentType("application/json")
-                        .content(userJson)
-                        .session(session))
+                        .content(userJson))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Login successful"));
+                .andExpect(content().string("Login successful"))
+                .andReturn();
+
+        String sessionId = loginResult.getResponse().getCookie("SESSION").getValue();
 
         mockMvc.perform(get("/api/v1/users")
                         .contentType("application/json")
-                        .session(session))
+                        .cookie(new Cookie("SESSION", sessionId)))
                 .andExpect(status().isOk());
     }
 
@@ -135,18 +170,18 @@ public class UserControllerTest {
                         .content(userJson))
                 .andExpect(status().isOk());
 
-        MockHttpSession session = new MockHttpSession();
-
-        mockMvc.perform(post("/api/v1/auth/login")
+        MvcResult loginResult = mockMvc.perform(post("/api/v1/auth/login")
                         .contentType("application/json")
-                        .content(userJson)
-                        .session(session))
+                        .content(userJson))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Login successful"));
+                .andExpect(content().string("Login successful"))
+                .andReturn();
+
+        String sessionId = loginResult.getResponse().getCookie("SESSION").getValue();
 
         String response = mockMvc.perform(get("/api/v1/mypage")
                         .contentType("application/json")
-                        .session(session))
+                        .cookie(new Cookie("SESSION", sessionId)))
                 .andExpect(status().isOk())
                 .andReturn().getResponse().getContentAsString();
 
