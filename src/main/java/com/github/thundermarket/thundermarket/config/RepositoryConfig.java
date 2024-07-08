11bac176fe4876295e3ddd9c4d01fb9c8aab9583
jjkim01 <jaen6563@gmail.com>
2024-07-08 19:09:11 +0900

    feature: 네이버 클라우드 플랫폼 object storage 사용을 위한 의존성, 상수, bean config 추가

--- src/main/java/com/github/thundermarket/thundermarket/config/RepositoryConfig.java
+++ src/main/java/com/github/thundermarket/thundermarket/config/RepositoryConfig.java
@@ -1,7 +1,7 @@
 package com.github.thundermarket.thundermarket.config;
 
-import com.github.thundermarket.thundermarket.repository.MySQLUserRepository;
-import com.github.thundermarket.thundermarket.repository.UserRepository;
+import com.github.thundermarket.thundermarket.repository.*;
+import com.github.thundermarket.thundermarket.service.ProductService;
 import com.github.thundermarket.thundermarket.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,5 +25,22 @@ public class RepositoryConfig {
     @Bean
     public UserRepository userRepository() {
         return new MySQLUserRepository(dataSource);
+    }
+
+    @Bean
+    public FileStorage fileStorage() {
+        return new NCloudStorage();
+    }
+
+    @Bean
+    public ProductService productService(
+            ProductRepository productRepository,
+            ProductDetailRepository productDetailRepository
+    ) {
+        return new ProductService(
+                productRepository,
+                productDetailRepository,
+                fileStorage()
+        );
     }
 }
--- src/main/java/com/github/thundermarket/thundermarket/constant/FileStorageConst.java
+++ src/main/java/com/github/thundermarket/thundermarket/constant/FileStorageConst.java
@@ -7,4 +7,6 @@ public class FileStorageConst {
     public static final int MAX_FILE_SIZE = 200 * 1024 * 1024; // 20MB
     public static final int MAX_VIDEO_LENGTH = 10; // 10 seconds
     public static final Set<String> ALLOWED_FILE_EXTENSIONS = Set.of("mp4", "avi", "mov"); // 화이트 리스트, 불변객체 사용
+    public static final String NCLOUD_BUCKET_NAME = "thunder-market-bucket";
+    public static final String NCLOUD_VIDEO_DIRECTORY = "video/";
 }
