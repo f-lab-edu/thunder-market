1ebcde26a51765fc5cb3259f17188b184bf74145
jjkim01 <jaen6563@gmail.com>
2024-07-21 01:05:46 +0900

    refactor: 불필요한 코드 삭제

--- src/test/java/com/github/thundermarket/thundermarket/UserControllerTest.java
+++ src/test/java/com/github/thundermarket/thundermarket/UserControllerTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -29,7 +28,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @Transactional
 @Testcontainers
-@ActiveProfiles("test")
 public class UserControllerTest {
 
     @Container
