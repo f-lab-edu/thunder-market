94ed431ada75c8945b61ced05f634a97112b109d
jjkim01 <jaen6563@gmail.com>
2024-07-21 01:03:15 +0900

    feature: spring data JDBC 적용을 위해 Repository에 CrudRepository 상속 추가하고 구체 클래스는 삭제

--- src/main/java/com/github/thundermarket/thundermarket/repository/ProductRepository.java
+++ src/main/java/com/github/thundermarket/thundermarket/repository/ProductRepository.java
@@ -2,13 +2,25 @@ package com.github.thundermarket.thundermarket.repository;
 
 import com.github.thundermarket.thundermarket.domain.Product;
 import com.github.thundermarket.thundermarket.domain.ProductFilterRequest;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface ProductRepository {
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long> {
 
-    List<Product> findAll(Long cursorId, int limit);
-    Product save(Product product);
-    void delete(Long id);
-    List<Product> filterByProductOptions(ProductFilterRequest productFilterRequest);
+    @Query("SELECT * FROM products WHERE id > :cursorId ORDER BY id DESC LIMIT :limit")
+    List<Product> findAllByIdGreaterThanOrderByIdDesc(Long cursorId, int limit);
+
+    @Query("SELECT p.* FROM products p JOIN productDetails pd ON p.id = pd.id " +
+            "WHERE (:#{#productFilterRequest.name} IS NULL OR p.name = :#{#productFilterRequest.name}) " +
+            "AND (:#{#productFilterRequest.priceMin} IS NULL OR p.price >= :#{#productFilterRequest.priceMin}) " +
+            "AND (:#{#productFilterRequest.priceMax} IS NULL OR p.price <= :#{#productFilterRequest.priceMax}) " +
+            "AND (:#{#productFilterRequest.color} IS NULL OR pd.color = :#{#productFilterRequest.color}) " +
+            "AND (:#{#productFilterRequest.purchaseDateMin} IS NULL OR pd.purchase_date >= :#{#productFilterRequest.purchaseDateMin}) " +
+            "AND (:#{#productFilterRequest.purchaseDateMax} IS NULL OR pd.purchase_date <= :#{#productFilterRequest.purchaseDateMax})")
+    List<Product> filterByProductOptions(@Param("productFilterRequest") ProductFilterRequest productFilterRequest);
 }
