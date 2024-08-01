dacbbf750a676f1df299222e7abb99289e43a783
jjkim01 <jaen6563@gmail.com>
2024-08-01 20:03:23 +0900

    feature: ProductCommandHandler 서비스 클래스 추가

---
+++ src/main/java/com/github/thundermarket/thundermarket/service/ProductCommandHandler.java
@@ -0,0 +1,46 @@
+package com.github.thundermarket.thundermarket.service;
+
+import com.github.thundermarket.thundermarket.domain.*;
+import com.github.thundermarket.thundermarket.exception.ResourceNotFoundException;
+import com.github.thundermarket.thundermarket.repository.FileStorage;
+import com.github.thundermarket.thundermarket.repository.ProductDetailRepository;
+import com.github.thundermarket.thundermarket.repository.ProductRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+@Transactional
+public class ProductCommandHandler {
+
+    private final ProductRepository productRepository;
+    private final ProductDetailRepository productDetailRepository;
+    private final FileStorage fileStorage;
+
+    public ProductCommandHandler(ProductRepository productRepository, ProductDetailRepository productDetailRepository, FileStorage fileStorage) {
+        this.productRepository = productRepository;
+        this.productDetailRepository = productDetailRepository;
+        this.fileStorage = fileStorage;
+    }
+
+    public ProductResponse add(Product product, ProductDetail productDetail, MultipartFile video) throws IOException {
+        Product savedProduct = productRepository.save(product);
+        FileUploadResult fileUploadResult = fileStorage.save(video);
+        ProductDetail productDetailWithVideoAndThumbnail = new ProductDetail.Builder(productDetail).withProductId(savedProduct.getId()).withVideo(fileUploadResult.getVideoFilePath()).withThumbnailFilePath(fileUploadResult.getThumbnailFilePath()).build();
+        return ProductResponse.of(savedProduct, productDetailRepository.save(productDetailWithVideoAndThumbnail));
+    }
+
+    public Product update(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
+}
