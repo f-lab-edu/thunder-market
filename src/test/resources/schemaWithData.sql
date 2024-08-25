d06513485d2ad9e30262f18d80a531a60f550657
jjkim01 <jaen6563@gmail.com>
2024-08-25 22:46:10 +0900

    test: 테스트용 sql에 comments 관련 테이블 추가

--- src/test/resources/schemaWithData.sql
+++ src/test/resources/schemaWithData.sql
@@ -40,6 +40,16 @@ create table keywords
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+create table comments
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    text VARCHAR(255) NOT NULL,
+    user_id BIGINT,
+    product_id BIGINT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
 INSERT INTO `users` (id, email, password)
 VALUES
     (1, 'jaen6563@naver.com', 'password');
